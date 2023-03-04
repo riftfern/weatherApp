4460314994e90d3ff50239813dcbded8588876e5
riftfern <adriftfern@gmail.com>
2023-03-04 15:31:45 -0500

    updated styles, added responsiveness, linted code

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const list = document.querySelector('api-section .cities')
+const list = document.querySelector("api-section .cities");
 const weatherApp = async (url) => {
-    const response = await fetch(url);
-    const WEATHER_DATA = await response.json();
+  const response = await fetch(url);
+  const WEATHER_DATA = await response.json();
 
-    const {main, name, sys, weather} = WEATHER_DATA;
+  const { main, name, sys, weather } = WEATHER_DATA;
 
-    const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;  
+  const icon = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
 
-    const li = document.createElement('li');
-    li.classList.add('city');
+  const li = document.createElement("li");
+  li.classList.add("city");
 
-    const markup = `
+  const markup = `
         <h2 class="city-name" data-name="${name},${sys.country}">
             <span>${name}</span>
             <sup>${sys.country}</sup>
         </h2>
-        <div class="city-temp">${(Math.round(main.temp) * 9/5 + 32)}<sup>°F</sup></div>
+        <div class="city-temp">${
+          (Math.round(main.temp) * 9) / 5 + 32
+        }<sup>°F</sup></div>
         <figure>
-            <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-            <figcaption>${weather[0]["description"]}</figcaption>
+            <img class="city-icon" src=${icon} alt=${weather[0].main}>
+            <figcaption>${weather[0].description}</figcaption>
         </figure> 
     `;
 
-    list.innerHTML = markup;
-    list.appendChild(li);
-}
-
+  list.innerHTML = markup;
+  list.appendChild(li);
+};
